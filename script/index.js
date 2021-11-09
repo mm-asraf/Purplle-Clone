dac22f4a31decab3ab755d2a4fbd6ddceb1e5858
Shivam Sharma <shivampandit635@gmail.com>
2021-11-09 12:17:41 +0530

    authenticate

--- script/index.js
+++ script/index.js
@@ -1,33 +1,37 @@
 
+
+
+//scrollbar start
+var counter = 1;
+setInterval(function () {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 3000)
+//scrollbar end
+
+//set timeout start
 var i = 0;
+var time = 1500;
 var images = [];
-var time = 4000;
-
-
-images[0] = '/images/web1.gif';
-images[1] = '/images/web2.gif';
-images[2] = '/images/web3.jpg';
-images[3] = '/images/web4.jpg';
-
-images[4] = '/images/web1.gif';
+images[0] = './img/web1.gif';
+images[1] = './img/web2.gif';
+images[2] = './img/web3.jpg';
+images[4] = './img/web4.jpg';
 
 function changeImg() {
-    document.sliding.src = images[i];
-
-
+    document.slide.src = images[i];
     if (i < images.length - 1) {
         i++;
     } else {
         i = 0;
     }
-
     setTimeout("changeImg()", time);
 }
+window.onload = changeImg
+//set - timeout end
 
-window.onload = changeImg;
+// featured
 
-
-// document.body.style.backgroundColor = "green"
-
-
-// signup
