f797b444ab32db512747e130ab43b2fb1b3cfed5
Shivam Sharma <shivampandit635@gmail.com>
2021-11-12 16:35:00 +0530

    signin

--- script/index.js
+++ script/index.js
@@ -1,37 +1,74 @@
 
+    var modal = document.getElementById("myModal")
+var span = document.getElementsByClassName("close")[0]
 
-
-//scrollbar start
-var counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 4) {
-        counter = 1;
-    }
-}, 3000)
-//scrollbar end
-
-//set timeout start
-var i = 0;
-var time = 1500;
-var images = [];
-images[0] = './img/web1.gif';
-images[1] = './img/web2.gif';
-images[2] = './img/web3.jpg';
-images[4] = './img/web4.jpg';
-
-function changeImg() {
-    document.slide.src = images[i];
-    if (i < images.length - 1) {
-        i++;
-    } else {
-        i = 0;
-    }
-    setTimeout("changeImg()", time);
+function signIn(){
+        modal.style.display = "block";
+        modal2.style.display = "none";
 }
-window.onload = changeImg
-//set - timeout end
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+// signup window
+var modal2 = document.getElementById("myModal2")
+var span2 = document.getElementsByClassName("close2")[0]
 
-// featured
+function signUp(){
+        modal2.style.display = "block";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+      // signup
 
+
+      function createAcc(e){
+
+e.preventDefault();
+let myForm = document.getElementById("signup-form");
+let name = myForm.name.value
+let email = myForm.email.value
+let mobile  = myForm.mobile.value
+let password = myForm.password.value
+console.log("data:",name,email, mobile, password);
+
+
+if (localStorage.getItem('purpleDataBase')=== null){
+localStorage.setItem("purpleDataBase", JSON.stringify([]));
+}
+let user ={
+name: name,
+email: email,
+mobile: mobile,
+password: password,
+};
+
+let arr = JSON.parse(localStorage.getItem("purpleDataBase"))
+
+arr.push(user);
+window.location.href ="demo.html"
+
+localStorage.setItem("purpleDataBase", JSON.stringify(arr))
+
+}
+    // login
+    
+    function Login(e){
+
+e.preventDefault();
+     let Lform = document.getElementById("Lform");
+     let mobile = Lform.mobile.value
+
+ let all_users =JSON.parse(localStorage.getItem("purpleDataBase"));
+
+ all_users.forEach(function (purpleDataBase) {
+     if(mobile === purpleDataBase.mobile ){
+      window.location.href ="demo.html"
+     }
+     else{
+       signUp()
+     }
+     
+ });
+
+}
