b03d088745ecee54d5bef6665321d0f59c7c0fa8
Shivam Sharma <shivampandit635@gmail.com>
2021-11-12 17:59:11 +0530

    validation

--- script/index.js
+++ script/index.js
@@ -22,53 +22,119 @@ span2.onclick = function() {
       // signup
 
 
-      function createAcc(e){
-
-e.preventDefault();
-let myForm = document.getElementById("signup-form");
-let name = myForm.name.value
-let email = myForm.email.value
-let mobile  = myForm.mobile.value
-let password = myForm.password.value
-console.log("data:",name,email, mobile, password);
-
-
-if (localStorage.getItem('purpleDataBase')=== null){
-localStorage.setItem("purpleDataBase", JSON.stringify([]));
-}
-let user ={
-name: name,
-email: email,
-mobile: mobile,
-password: password,
-};
-
-let arr = JSON.parse(localStorage.getItem("purpleDataBase"))
-
-arr.push(user);
-window.location.href ="demo.html"
-
-localStorage.setItem("purpleDataBase", JSON.stringify(arr))
-
-}
+      function validEmail() {
+        let email = document.getElementById('email').value;
+        let password = document.getElementById("password").value;
+    
+        let obj = {};
+        if(email.length === 0){
+            alert('Email should not be empty!');
+            return;
+        }
+        for(let i = 0; i < email.length; i++){
+            if(obj[email[i]] === undefined){
+                obj[email[i]] = 1;
+            }
+            else{
+                obj[email[i]] += 1;
+            }
+        }
+        if(obj[' '] !== undefined){
+            alert('Email should not contain any white-spaces!');
+            return false;
+        }
+        if(obj['@'] !== 1){
+            alert('Email should contain exactly 1 "@"!');
+            return;
+        }
+        if(obj['.'] !== 1){
+            alert('Email should contain exactly 1 "."');
+            return;
+        }
+    
+        let specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        let obj2 = {};
+        let flag = false;
+        if(password.length < 5){
+            alert('Password should not be empty or it must be minimum 5 char!');
+            return;
+        }
+        for(let i = 0; i < password.length; i++){
+            for(let j = 0; j < specialChar.length;  j++){
+                if(password[i] === specialChar[j]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag)
+                break;
+        }
+        for(let i = 0; i < users.length; i++){
+            if(arr[i].email === email){
+                alert("User allready exist!")
+                
+                return;
+            }
+        }
+        if(flag !== true){
+            alert('Password must coantain one special char!');
+            return false;
+        }
+        
+      else{
+        createAcc(event);
+      }
+    }
+          function createAcc(e){
+    
+    e.preventDefault();
+    let myForm = document.getElementById("signup-form");
+    let name = myForm.name.value
+    let email = myForm.email.value
+    let mobile  = myForm.mobile.value
+    let password = myForm.password.value
+    console.log("data:",name,email, mobile, password);
+    
+    
+    if (localStorage.getItem('purpleDataBase')=== null){
+    localStorage.setItem("purpleDataBase", JSON.stringify([]));
+    }
+    
+    
+    let user ={
+    name: name,
+    email: email,
+    mobile: mobile,
+    password: password,
+    };
+    
+    let arr = JSON.parse(localStorage.getItem("purpleDataBase"))
+    
+    arr.push(user);
+    window.location.href ="index.html"
+    
+    localStorage.setItem("purpleDataBase", JSON.stringify(arr))
+    
+    }
     // login
     
+     
     function Login(e){
 
-e.preventDefault();
-     let Lform = document.getElementById("Lform");
-     let mobile = Lform.mobile.value
-
- let all_users =JSON.parse(localStorage.getItem("purpleDataBase"));
-
- all_users.forEach(function (purpleDataBase) {
-     if(mobile === purpleDataBase.mobile ){
-      window.location.href ="demo.html"
-     }
-     else{
-       signUp()
-     }
-     
- });
-
-}
+        e.preventDefault();
+             let Lform = document.getElementById("Lform");
+             let mobile = Lform.mobile.value
+        
+         let all_users =JSON.parse(localStorage.getItem("purpleDataBase"));
+        
+         all_users.forEach(function (purpleDataBase) {
+             if(mobile === purpleDataBase.mobile ){
+              window.location.href ="index.html"
+             }
+             else{
+               signUp()
+             }
+             
+         });
+        
+        }
